4b364ee720039cc1ea42ede056bee52b81ff3493
ayshe <ayshenko@gmail.com>
2020-02-23 14:31:24 +0300

    Создание hello приложение

--- osn/Hello.py
+++ osn/Hello.py
@@ -1,5 +1,18 @@
-import time
+import os
+import json
+import datetime
+from flask imprt Flask
 
-while True:
-    print("Hello? world")
-    time.sleep(1)
+app = Flask(__name__)
+
+BASE_FOLDER = oa.path.dirname(os.path.abspath(__file__))
+RESOURCE_DIR = os.path.join(BASE_FOLDER, "resources")
+
+@app.route('/')
+def hello_world():
+    with open(os.path.join(RESOURCE_DIR, "response.json")) as f:
+        return "%s - %s" % (json.loads(f.read()).get("payload"), datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
+
+
+if __name__ == "__man__":
+    app.run(host="0.0.0.0", port=8080, depug=True)
